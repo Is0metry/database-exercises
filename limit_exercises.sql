5d2a826a5710e70e4d0c08125a7f60c1a3f65448
Is0metry <sims.woodrow@gmail.com>
2022-09-07 14:35:54 -0500

    Adding limit/offset exercises.

---
+++ limit_exercises.sql
@@ -0,0 +1,36 @@
+USE employees;
+
+-- q2
+SELECT DISTINCT last_name FROM employees ORDER BY last_name LIMIT 10;
+/*
+Aamodt
+Acton
+Adachi
+Aingworth
+Akaboshi
+Akazan
+Akiyama
+Alameldin
+Albarhamtoshy
+Alblas
+*/
+-- q3
+SELECT first_name, last_name FROM employees WHERE hire_date LIKE '199%-%-%' AND birth_date LIKE '%-12-31' ORDER BY hire_date LIMIT 5;
+/*
+Magdalena	Guting
+Chaosheng	Plavsic
+Sachin	    Brodie
+Douad       Streit
+Ymte	    Schlegelmilch
+*/
+-- q4 
+SELECT first_name, last_name FROM employees WHERE hire_date LIKE '199%-%-%' AND birth_date LIKE '%-12-31' ORDER BY hire_date LIMIT 5 OFFSET 45;
+/*
+Tomofumi	Ducloy
+Willard	    Itschner
+Ziyad	    Schueller
+Claude	    Ventosa
+Alenka	    Garrabrants
+
+OFFSET = Page_Number * LIMIT
+*/
